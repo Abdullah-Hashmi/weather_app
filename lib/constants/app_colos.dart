7eb26fe850276d1771735a244fbabb8fc199fe3f
Syed Abdullah <syedabdullah.developer@gmail.com>
2024-12-13 17:42:09 +0500

    Initial commit

---
+++ lib/constants/app_colos.dart
@@ -0,0 +1,9 @@
+import "package:flutter/material.dart";
+
+@immutable
+class AppColors {
+  //? Blues
+  static const Color darkBlue = Color(0xFF002B5B);
+  static const Color lightBlue = Color(0xFF0077B6);
+  static const Color accentBlue = Color(0xFF23224A);
+}
